be7ca11d313740e370df77050c6907fb98d5f540
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-09 15:22:57 -0400

    Setting up components

---
+++ clickygame/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron () {
+    return(
+        <div class="jumbotron">
+ 
+         <h1 class="text-center">Clicky Game!</h1>
+ 
+         <h2 class="text-center">Click on an image to earn points, but don't click on any more than once!</h2>
+ 
+     </div>
+    )
+}
---
+++ clickygame/src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar () {
+    return (
+        <nav class="navbar">
+  <div class="container">
+     <ul>
+         <li class="brand">Clicky Game</li>
+         <li>Click an image to begin!</li>
+         <li>Score:  | Top: </li>
+     </ul>
+ </div>
+ </nav>
+    )
+}
+
+export default Navbar;
